a9df7509f26d8f25f8a986aa1b93e4fb377f3a44
gittest <kaminer_sol2@naver.com>
2024-06-27 16:54:45 +0900

    240627 commit

--- myportal/src/main/java/himedia/myportal/repositories/dao/BoardDao.java
+++ myportal/src/main/java/himedia/myportal/repositories/dao/BoardDao.java
@@ -7,10 +7,10 @@ import himedia.myportal.repositories.vo.BoardVo;
 public interface BoardDao {
 	public List<BoardVo> selectAll();
 	public int insert(BoardVo vo); // 게시물 작성
-	public int delete(BoardVo vo);
+//	public int delete(BoardVo vo);
 	public BoardVo getContent(Long no);
 	public int update(BoardVo vo);
-	public BoardVo getContent(BoardVo boardvo);
+//	public BoardVo getContent(BoardVo boardvo);
 	int increaseHitCount(Long no);
 	public int delete(Long no, Long userNo);
 }
